2b96089e80a80f69a74053bc6607a9da35f0fc9e
meywood <ian@meywood.com>
2022-07-12 11:39:11 +0200

    Move top level package over from com.syntifi.casper to com.casper

---
+++ src/main/java/com/casper/sdk/model/clvalue/cltype/CLTypeTuple2.java
@@ -0,0 +1,37 @@
+package com.casper.sdk.model.clvalue.cltype;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.casper.sdk.exception.NoSuchTypeException;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+/**
+ * CLType for {@link AbstractCLType#TUPLE2}
+ *
+ * @author Alexandre Carvalho
+ * @author Andre Bertolace
+ * @see AbstractCLType
+ * @since 0.0.1
+ */
+@Getter
+@EqualsAndHashCode(callSuper = true, of = {"typeName"})
+public class CLTypeTuple2 extends AbstractCLTypeWithChildren {
+    private final String typeName = TUPLE2;
+
+    @Override
+    @JsonProperty(TUPLE2)
+    protected void setChildTypeObjects(List<Object> childTypeObjects)
+            throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException,
+            NoSuchMethodException, SecurityException, NoSuchTypeException {
+        super.setChildTypeObjects(childTypeObjects);
+    }
+
+    @Override
+    @JsonProperty(TUPLE2)
+    protected List<Object> getChildTypeObjects() {
+        return super.getChildTypeObjects();
+    }
+}
